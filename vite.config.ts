1b1f901d8a92b5cf529d8d9557ab00c3edaf0750
ehu shubham shaw <ehushubham@gmail.com>
2025-01-14 01:18:59 -0500

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
   server: {
     host: '0.0.0.0',
     port: 3000,
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+    open: true, // Automatically opens the browser
+    cors: true, // Enable CORS for debugging API issues
+    strictPort: true, // Ensures the server exits if the port is unavailable
+  },
 })
